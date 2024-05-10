05852a5ba1197af4a4e798b39666a605ad264973
Leo <leonel.m.cata03@gmail.com>
2024-05-10 15:19:06 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Stopwatch from "./components/Stopwatch";
+
+const App = () => {
+  return (
+    <div className="h-screen bg-pink-300">
+      <Stopwatch />
+    </div>
+  );
+};
+
+export default App;
